02b79fab2b9aa9d33b8731e3534f8af935141e9a
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-04-02 18:56:46 -0400

    Add servos initialization
    Co-authored-by: Brady Flynn <computerguy980@users.noreply.github.com>
    Co-authored-by: Grigory Shvechkov <grigory-shvechkov@users.noreply.github.com>

--- main.ino
+++ main.ino
@@ -1,7 +1,25 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+// define servo pins
+Servo servos[4][3];
+const int servoPins[4][3] = {
+    {5, 18, 19},
+    {21, 22, 23},
+    {34, 35, 32},
+    {33, 25, 26},
+};
 
 void setup() {
     Serial.begin(115200);
+
+    // initialize servos
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            servos[i][j].attach(servoPins[i][j]);
+            servos[i][j].write(90); // set initial position to 90 degrees
+        }
+    }
 }
 
 void loop() {
