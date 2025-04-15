f880883b76859c4927e62916e2e4b9c268ce0a05
Greg <gshvechkov@umass.edu>
2025-04-15 01:13:57 -0400

    New software

--- main.ino
+++ main.ino
@@ -1,84 +1,156 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-#define MIN_ANGLE_LEFT_HIP  90
-#define MAX_ANGLE_LEFT_HIP  180
-#define MIN_ANGLE_LEFT_KNEE 0
-#define MAX_ANGLE_LEFT_KNEE 90
-#define MIN_ANGLE_LEFT_ANKLE 80
-#define MAX_ANGLE_LEFT_ANKLE 180
-#define MIN_ANGLE_RIGHT_HIP 0
-#define MAX_ANGLE_RIGHT_HIP 90
-#define MIN_ANGLE_RIGHT_KNEE 90
-#define MAX_ANGLE_RIGHT_KNEE 180
-#define MIN_ANGLE_RIGHT_ANKLE 0
-#define MAX_ANGLE_RIGHT_ANKLE 100
+// based on range of motion
+#define HIP_MAX 90
+#define HIP_MIN 0
 
-class Joint {
-    Servo servo;
-    int pin;
-    int minAngle;
-    int maxAngle;
-    public:
-        Joint(int pin, int minAngle, int maxAngle) {
-            servo.attach(pin);
-            servo.write(minAngle + (maxAngle - minAngle) / 2); // set initial position to the middle
+#define KNEE_MIN 180
+#define KNEE_MAX 90
+
+#define ANKLE_MAX 90
+#define ANKLE_MIN 180
+
+enum joint
+{
+    HIP = 1,
+    KNEE = 2,
+    ANKLE = 3,
+};
+
+class Leg
+{
+    Servo hip;
+    Servo knee;
+    Servo ankle;
+
+public:
+    Leg(int HIP_PIN, int KNEE_PIN, int ANKLE_PIN)
+    {
+
+        this->hip.attach(HIP_PIN);
+        delay(100); // Delay for servo initialization
+        this->knee.attach(KNEE_PIN);
+        delay(100);
+        this->ankle.attach(ANKLE_PIN);
+    }
+
+    //returns a requested joint angle
+    int joint_ang(int joint)
+    {
+        switch (joint)
+        {
+        case HIP:
+            return this->hip.read();
+            break;
+
+        case KNEE:
+            return this->knee.read();
+            break;
+
+        case ANKLE:
+            return this->ankle.read();
+            break;
+
+        default:
+            return 0;
         }
+    }
+    //moves all joints to specificed angle
+    void move(int HIP_ANGLE, int KNEE_ANGLE, int ANKLE_ANGLE)
+    {
+        Serial.println("Moved");
+        this->hip.write(HIP_ANGLE);
+        this->knee.write(KNEE_ANGLE);
+        this->ankle.write(ANKLE_ANGLE);
+    }
 
-        void move(int angle) {
-            if (angle < minAngle) {
-                Serial.printf("Angle is less than %d degrees, setting to %d degrees.\n", minAngle, minAngle);
-                angle = minAngle;
-            } else if (angle > maxAngle) {
-                Serial.printf("Angle is greater than %d degrees, setting to %d degrees.\n", maxAngle, maxAngle);
-                angle = maxAngle;
-            }
-            servo.write(angle);
+    //prints every joints current angle
+    void read()
+    {
+        Serial.println(hip.read());
+        Serial.println(knee.read());
+        Serial.println(ankle.read());
+    }
+};
+
+class Spider
+{
+    Leg left_front;
+    Leg left_rear;
+    Leg right_front;
+    Leg right_rear;
+
+public:
+    Spider(Leg lf_param, Leg lr_param, Leg rf_param, Leg rr_param)
+        : left_front(lf_param), left_rear(lr_param), right_front(rf_param), right_rear(rr_param) {}
+
+    //move selected leg to given angles in respective order
+    void move_leg(int leg_num, int HIP_ANGLE, int KNEE_ANGLE, int ANKLE_ANGLE)
+    {
+        switch (leg_num)
+        {
+        case 1:
+            this->left_front.move(HIP_ANGLE, KNEE_ANGLE, ANKLE_ANGLE);
+            break;
+        case 2:
+            this->left_rear.move(HIP_ANGLE, KNEE_ANGLE, ANKLE_ANGLE);
+            break;
+        case 3:
+            this->right_front.move(HIP_ANGLE, KNEE_ANGLE, ANKLE_ANGLE);
+            break;
+        case 4:
+            this->right_rear.move(HIP_ANGLE, KNEE_ANGLE, ANKLE_ANGLE);
+            break;
+        default:
+            return;
         }
-};
-struct Leg {
-    Joint hip;
-    Joint knee;
-    Joint ankle;
-};
-struct Legs {
-    Leg frontLeft;
-    Leg frontRight;
-    Leg rearleft;
-    Leg rearRight;
+    }
+
+    void stand_up()
+    {   
+
+        left_front.move(HIP_MAX,KNEE_MIN,ANKLE_MAX);
+    }
+
+    void forward()
+    {
+        // Implement forward movement
+    }
 };
 
-Legs *legs;
+Leg *left_front;
+Leg *left_rear;
+Leg *right_front;
+Leg *right_rear;
 
-void setup() {
+Spider *spider;
+
+Servo test_servo;
+
+void setup()
+{
+    /*
+    //setup serial communication
     Serial.begin(115200);
+    Serial.println("TIME TO CRAWL");
 
-    // initialize servos
-    legs = new Legs {
-        .frontLeft = {
-            .hip = Joint(32, MIN_ANGLE_LEFT_HIP, MAX_ANGLE_LEFT_HIP),
-            .knee = Joint(35, MIN_ANGLE_LEFT_KNEE, MAX_ANGLE_LEFT_KNEE),
-            .ankle = Joint(34, MIN_ANGLE_LEFT_ANKLE, MAX_ANGLE_LEFT_ANKLE),
-        },
-        .frontRight = {
-            .hip = Joint(23, MIN_ANGLE_RIGHT_HIP, MAX_ANGLE_RIGHT_HIP),
-            .knee = Joint(22, MIN_ANGLE_RIGHT_KNEE, MAX_ANGLE_RIGHT_KNEE),
-            .ankle = Joint(21, MIN_ANGLE_RIGHT_ANKLE, MAX_ANGLE_RIGHT_ANKLE),
-        },
-        .rearleft = {
-            .hip = Joint(33, MIN_ANGLE_LEFT_HIP, MAX_ANGLE_LEFT_HIP),
-            .knee = Joint(25, MIN_ANGLE_LEFT_KNEE, MAX_ANGLE_LEFT_KNEE),
-            .ankle = Joint(26, MIN_ANGLE_LEFT_ANKLE, MAX_ANGLE_LEFT_ANKLE),
-        },
-        .rearRight = {
-            .hip = Joint(19, MIN_ANGLE_RIGHT_HIP, MAX_ANGLE_RIGHT_HIP),
-            .knee = Joint(18, MIN_ANGLE_RIGHT_KNEE, MAX_ANGLE_RIGHT_KNEE),
-            .ankle = Joint(5, MIN_ANGLE_RIGHT_ANKLE, MAX_ANGLE_RIGHT_ANKLE),
-        },
-    };
+    //create four leg objects
+    left_front = new Leg(34, 35, 32); // issue with hardwaare with this leg
+    left_rear = new Leg(26, 25, 33);
+    right_front = new Leg(23, 22, 21);
+    right_rear = new Leg(5, 18, 19);
+    //create new spider object
+    spider = new Spider(*left_front, *left_rear, *right_front, *right_rear); // give spider the leg objects
+    */
+    test_servo.attach(35);
 }
 
-void loop() {
-    Serial.println("Hello World!");
+void loop()
+{   
+    test_servo.write(90);
     delay(1000);
+    test_servo.write(0);
+    delay(1000);
+    //loop code goes here
 }
