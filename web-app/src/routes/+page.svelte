999d38d8f77c20e954aec5a1a8a9494248abb9ed
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-04-23 21:43:44 -0400

    Add forward button

--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+    import ChevronUp from "@lucide/svelte/icons/chevron-up";
+
+    async function handleForwardClick() {
+        const response = await fetch("http://192.168.4.1/rotate?leg=fl&joint=hip&angle=45", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (response.ok) {
+            const result = await response.json();
+            console.log("Success: ", result);
+        } else {
+            console.log("Error: ", response);
+        };
+    };
+</script>
+
+<div class="flex flex-col items-center gap-8 p-8">
+    <h1 class="text-center text-4xl font-semibold">Robot Spider Control</h1>
+    <div class="flex flex-col">
+        <Button onclick={handleForwardClick} size="icon">
+            <ChevronUp />
+        </Button>
+    </div>
+</div>
