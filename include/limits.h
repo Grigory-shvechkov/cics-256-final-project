6ab0d1c09a1036b49809f384a4089bfeb666e292
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-04-24 21:36:58 -0400

    Fix offsets

--- include/limits.h
+++ include/limits.h
@@ -3,7 +3,7 @@
 #define FL_HIP_MAX 90
 #define FL_KNEE_MIN 50
 #define FL_KNEE_MAX 180
-#define FL_ANKLE_MIN 90
+#define FL_ANKLE_MIN 0
 #define FL_ANKLE_MAX 180
 
 // Front right limits
@@ -12,7 +12,7 @@
 #define FR_KNEE_MIN 0
 #define FR_KNEE_MAX 130
 #define FR_ANKLE_MIN 0
-#define FR_ANKLE_MAX 90
+#define FR_ANKLE_MAX 180
 
 // Rear left limits
 #define RL_HIP_MIN 90
@@ -20,12 +20,12 @@
 #define RL_KNEE_MIN 0
 #define RL_KNEE_MAX 130
 #define RL_ANKLE_MIN 0
-#define RL_ANKLE_MAX 90
+#define RL_ANKLE_MAX 180
 
 // Rear right limits
 #define RR_HIP_MIN 0
 #define RR_HIP_MAX 90
 #define RR_KNEE_MIN 50
 #define RR_KNEE_MAX 180
-#define RR_ANKLE_MIN 90
+#define RR_ANKLE_MIN 0
 #define RR_ANKLE_MAX 180
